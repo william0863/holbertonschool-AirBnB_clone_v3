40b9b957492fdd2e5bafeb533654a2b46cb3a4b9
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 <l.leroty@gmail.com>
2022-12-06 20:29:54 +0100

    add task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -10,3 +17,20 @@ def app_views_status():
     """returns status"""
     dictionary = {"status": "OK"}
     return jsonify(dictionary)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def endpoint_stats():
+    """
+    Create an endpoint that retrieves the number of each objects by
+    type
+    """
+
+    # dictionary of classes + path of count method to use it
+    classes_dict = {"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)}
+    return classes_dict
