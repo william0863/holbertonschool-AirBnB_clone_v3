7d69059b4dcc0b9a251834dbc8a649c7803929ca
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 <l.leroty@gmail.com>
2022-12-08 10:52:51 +0100

    add the cities file

---
+++ api/v1/views/cities.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""view of State object"""
+
+from api.v1.views import app_views
+from models import storage
+from flask import jsonify, abort, request
+from models.state import State
+from models.city import City
+
+
+@app_views.route('/states/<state_id>/cities', methods=["GET"],
+                 strict_slashes=False)
+def get_all_cities(state_id=None):
+    """return all cities objects"""
+    state_list = []
+    all_objs = storage.all(State)
+    obj = storage.get("State", state_id)
+    for obj in all_objs.values():
+        state_list.append(obj.to_dict())
+    if obj is None:
+        abort(404)
+    return jsonify(state_list)
+
+
+@app_views.route('/states/<city_id>', methods=["GET"])
+def get_city(city_id=None):
+    """return json City object"""
+    obj = storage.get("City", city_id)
+    if obj is None:
+        abort(404)
+    else:
+        return jsonify(obj.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=["DELETE"])
+def city_delete(city_id=None):
+    """delete an object by id"""
+    obj = storage.get("City", city_id)
+    if obj is None:
+        abort(404)
+    storage.delete(obj)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states/', methods=["POST"], strict_slashes=False)
+def post_obj_city():
+    """add new state object"""
+    dic = {}
+    dic = request.get_json(silent=True)
+    if dic is None:
+        abort(400, "Not a JSON")
+    if "name" not in dic.keys():
+        abort(400, "Missing name")
+    new_state = storage.all(City)
+    for k, v in dic.items():
+        setattr(new_state, k, v)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=["PUT"], strict_slashes=False)
+def update_obj_city(city_id=None):
+    """update new state object"""
+    dic = {}
+    obj = storage.get("City", city_id)
+    if obj is None:
+        abort(404)
+    dic = request.get_json(silent=True)
+    if dic is None:
+        abort(400, "Not a JSON")
+    for key, value in dic.items():
+        setattr(obj, key, value)
+    storage.save()
+    return jsonify(obj.to_dict()), 200
